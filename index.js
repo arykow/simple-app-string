7503148f6631d0aa8f3a6e8ec98ee0389738b917
Karim DRIDI <karim.dridi@gmail.com>
2021-03-18 11:57:23 +0100

    configure application port using envvars

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.post('/', (req, res) => {
 	res.send('Hello World!')
@@ -10,6 +9,7 @@ app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
